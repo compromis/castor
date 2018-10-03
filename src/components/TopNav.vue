8968a87a045a9570c1093de7e9252d971dae5bf9
Guillem <guillem@disedit.com>
2018-10-03 10:21:58 +0200

    Clean default vue-cli components

---
+++ src/components/TopNav.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Top nav
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNav'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered'))
 }).$mount('#app')
