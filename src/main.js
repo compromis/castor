d3adf5d501743f74bfeb9d2664bbf3294c35a448
Guillem <guillem@disedit.com>
2018-10-03 10:18:36 +0200

    Add prerenderer

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
